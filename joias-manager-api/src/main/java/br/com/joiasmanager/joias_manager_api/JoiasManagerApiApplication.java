5d9481dbe45329a888affdff24696df0509b9a6c
Paulo <pauloandresdsf@gmail.com>
2025-03-05 23:38:15 -0300

    create(projeto):criação usando spring initialzier e injeção de dependencias

---
+++ joias-manager-api/src/main/java/br/com/joiasmanager/joias_manager_api/JoiasManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.joiasmanager.joias_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoiasManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoiasManagerApiApplication.class, args);
+	}
+
+}
